7895a273774cd12d0295d1406102fd0b7db874c7
yinwuteng <470047349@qq.com>
2018-12-25 20:54:00 +0800

    Initial commit

---
+++ lib/ItemView.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_app/Building.dart';
+
+//定义回调接口
+typedef OnItemClickListener = void Function(int position);
+
+class ItemView extends StatelessWidget {
+  final int position;
+  final Building building;
+  final OnItemClickListener listener;
+
+  ItemView(this.position, this.building, this.listener);
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+
+    final icon = Icon(
+      building.type == BuildingType.restaurant
+          ? Icons.restaurant
+          : Icons.theaters,
+      color: Colors.blue[500],
+    );
+    final widget = Row(
+      children: <Widget>[
+        Container(
+          margin: EdgeInsets.all(16.0),
+          child: icon,
+        ),
+        Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text(
+                  building.title,
+                  style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.w500),
+                ),
+                Text(building.address)
+              ],
+            ))
+      ],
+    );
+    //设置监听事件
+    return InkWell(
+      onTap: () => listener(position),
+      child: widget,
+    );
+  }
+}
+
+class BuildingListView extends StatelessWidget {
+  final List<Building> buildings;
+  final OnItemClickListener listener;
+
+  BuildingListView(this.buildings,this.listener);
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    //listView.builder可以按需求生成子空间
+    return ListView.builder(
+        itemCount: buildings.length,
+        itemBuilder: (context, index) {
+          return new ItemView(index, buildings[index],listener);
+        });
+  }
+}
---
+++ lib/TestApp.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(new TestApp());
+
+class TestApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return MaterialApp(
+      title: 'ListVioew Demo',
+      home: FirstScreen(),
+    );
+  }
+}
+
+//第一个页面
+class FirstScreen extends StatefulWidget {
+  _FirstScreenState createState() => _FirstScreenState();
+}
+
+class _FirstScreenState extends State<FirstScreen> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Navigation demo'),
+      ),
+      body: Center(
+        child: RaisedButton(
+            child: Text('First screen'),
+            onPressed: () async {
+              var msg = await Navigator.push(
+                  context, MaterialPageRoute(builder: (_) => SecondScreen()),);
+              debugPrint('msg=$msg');
+            }),
+      ),
+    );
+  }
+}
+
+//第二个页面
+class SecondScreen extends StatefulWidget {
+  _SecondScreenState createState() => _SecondScreenState();
+}
+
+class _SecondScreenState extends State<SecondScreen> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Navigation demo'),
+      ),
+      body: Center(
+        child: RaisedButton(
+            child: Text('Second screen'),
+            onPressed: () {
+              Navigator.pop(context, 'message from second screen');
+            }),
+      ),
+    );
+  }
+}
